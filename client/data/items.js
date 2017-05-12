426bd6d24599da90fb6886854540b66fa71b75ec
James Gardiner <jamesgardiner075@gmail.com>
2017-05-12 14:31:12 +0100

    Add data

---
+++ client/data/items.js
@@ -0,0 +1,32 @@
+const items = [
+   {
+      "id":"1161022966406956503",
+      "name": "Tequila",
+      "currency":"£",
+      "amount":"1200.00",
+      "toggleEdit": false
+   },
+   {
+      "id":"1161022966406956504",
+      "name": "Salt",
+      "currency":"£",
+      "amount":"12.00",
+      "toggleEdit": false
+   },
+   {
+      "id":"1161022966406956505",
+      "name": "Lemon",
+      "currency":"£",
+      "amount":"240.00",
+      "toggleEdit": false
+   },
+   {
+      "id":"1161022966406956506",
+      "name": "Bartender",
+      "currency":"£",
+      "amount":"345.60",
+      "toggleEdit": false
+   }
+];
+
+export default items;
